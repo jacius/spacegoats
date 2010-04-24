e33cacd2929470787c3fe1997296f9e3eec2dd4f
John Croisant <jacius@gmail.com>
2010-04-24 16:41:24 -0500

    My boat can turn now.

--- src/boat.rb
+++ src/boat.rb
@@ -22,8 +22,42 @@ class Boat < Actor
                     [ 5, 15], [ 5,-15]]
   }
 
+  attr_reader :turning_left, :turning_right
+
   def setup
     self.action = :idle
+
+    @turn_rate = 5
+    @turn_decay = 0.2
+
+    @turning_left  = false
+    @turning_right = false
+
+    i = input_manager
+
+    i.reg( KeyPressed,  :left  ){  @turning_left  = true   }
+    i.reg( KeyReleased, :left  ){  @turning_left  = false  }
+
+    i.reg( KeyPressed,  :right ){  @turning_right = true   }
+    i.reg( KeyReleased, :right ){  @turning_right = false  }
+  end
+
+
+  def update( time_ms )
+
+    seconds = time_ms * 0.001
+
+    turn = 0
+    turn -= 1 if @turning_left
+    turn += 1 if @turning_right
+
+    if turn != 0
+      physical.body.w = @turn_rate * turn
+    else
+      physical.body.w *= @turn_decay ** seconds
+    end
+
+    super
   end
 
 end
