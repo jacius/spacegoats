d653527696f8eca093375ae042e5816904f5c8a2
John Croisant <jacius@gmail.com>
2010-04-24 00:11:05 -0500

    Generated Gamebox 0.1.0 app.

---
+++ src/main_stage.rb
@@ -0,0 +1,21 @@
+require 'stage'
+
+class MainStage < Stage
+  def setup
+    super
+    @my_actor = create_actor :my_actor
+    @my_actor.x = 10
+    @my_actor.y = 10
+
+    @stars = []
+    20.times { @stars << Ftor.new(rand(viewport.width),rand(viewport.height)) }
+  end
+
+  def draw(target)
+    target.fill [25,25,25,255]
+    for star in @stars
+      target.draw_circle_s([star.x,star.y],1,[255,255,255,255])
+    end
+    super
+  end
+end
